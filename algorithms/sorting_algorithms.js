0fc9028c38ad36b6c304be276b4456410c4fdc2c
Thomas Dailey <daileytj89@gmail.com>
2017-04-26 19:06:52 -0400

    sorting algorithm solutions

--- algorithms/sorting_algorithms.js
+++ algorithms/sorting_algorithms.js
@@ -1,4 +1,6 @@
-// Bubble Sort
+// Bubble Sort - Best Case O(n) if the array is already sorted
+// Worst Case O(n^2)if each value needs to be swapped with another value
+// note: very terrible way to sort an array!!
 
 function swap(array, i, j) {
     const tmp = array[i];
@@ -22,6 +24,7 @@ function bubbleSort(array) {
 };
 
 // Merge Sort
+// Best, worst, and average case of O(nlog(n))
 
 function mergeSort(array) {
     if (array.length <= 1) {
@@ -61,6 +64,8 @@ function merge(left, right, array) {
 };
 
 // Quicksort
+// Best Case - O(nlog(n))
+// Wrost Case - O(n^2)
 
 function quickSort(array, start=0, end=array.length) {
     start = start;
@@ -88,12 +93,75 @@ function partition(array, start, end) {
 };
 
 // Interview questions
+let test = [9, 1, 10, 7, 4, 8]
+let test2 = [ 1, 4, 7, 8, 9, 10 ]
+let books = ['In Search of Lost Time', 'Ulysses', 'Don Quixote', 'Moby Dick', 'Hamlet', 'War and Peace', 'The Odyssey', 'The Great Gatsby', 'The Divine Comedy', 'Madame Bovary', 'The Brothers Karamazov', 'One Hundred Years of Solitude', 'The Adventures of Huckleberry Finn', 'The Iliad', 'Lolita', 'Anna Karenina', 'Crime and Punishment', 'Alices Adventures in Wonderland', 'The Sound and the Fury', 'Pride and Prejudice'];
+
 
 // Write an O(n) algorithm to sort an array of integers, where you know in advance
 // what the lowest and highest values are.
+console.log("-------------------");
+console.log("Sort Int");
+console.log("-------------------");
+
+let sortInt = (array, low, high) => {
+	let temp = new Array(high)
+	let output = []
+	for (let i=0; i < array.length; i++) {
+		temp[array[i]] = array[i]
+	}
+	for (let i=0; i < temp.length; i++) {
+		if (temp[i]) {
+			output.push(temp[i])
+		}
+	}
+	return output
+}
+// account for duplicates e.g., sortInt([1,1,1,2,1,3,2,1,4,1,5,1,2,1,4,1,2,1,3], 1, 4);
+console.log(sortInt(test, 1, 10));
 
 // Write an algorithm to shuffle an array into a random order in-place (i.e. without
 // creating a new array).
+console.log("-------------------");
+console.log("Randomize");
+console.log("-------------------");
+
+let randomize = (array) => {
+	for (let i=0; i < array.length - 1; i++) {
+		let j = Math.floor(Math.random() * (array.length - (i + 1))) + (i + 1);
+		swap(array, i, j)
+	}
+	return array
+}
+
+console.log(randomize(test2));
 
 // Imagine that I gave you twenty books to sort in alphabetical order. How would you
 // go about it? Can you express this as an algorithm?
+
+console.log("-------------------");
+console.log("Sort Books");
+console.log("-------------------");
+
+let sortBooks = (array) => {
+	let output = [array[0]]
+
+	for (let i=1; i < array.length; i++) {
+		let book = array[i]
+		for (let j=0; j < output.length; j++) {
+			let bookComp = book.replace(/^(the|a) /i, '')
+			let outputComp = output[j].replace(/^(the|a) /i, '')
+			if (bookComp < outputComp) {
+				output.splice(j, 0, book)
+				break
+			}
+		}
+		if (output.length < i + 1) {
+			output.push(book)
+		}
+
+	}
+	return output
+}
+
+console.log(sortBooks(books));
